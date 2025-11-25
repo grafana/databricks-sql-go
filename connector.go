ab2c8a5aaa1c54e4a0893e113da4844f91bd57ed
multimo <tim.mulqueen@gmail.com>
2025-11-25 13:43:07 +0100

    add configuration option for http client for cloud config

--- connector.go
+++ connector.go
@@ -63,7 +63,16 @@ func (c *connector) Connect(ctx context.Context) (driver.Conn, error) {
 		CanUseMultipleCatalogs: &c.cfg.CanUseMultipleCatalogs,
 	})
 	if err != nil {
-		return nil, dbsqlerrint.NewRequestError(ctx, fmt.Sprintf("error connecting: host=%s port=%d, httpPath=%s", c.cfg.Host, c.cfg.Port, c.cfg.HTTPPath), err)
+		return nil, dbsqlerrint.NewRequestError(
+			ctx,
+			fmt.Sprintf(
+				"error connecting: host=%s port=%d, httpPath=%s",
+				c.cfg.Host,
+				c.cfg.Port,
+				c.cfg.HTTPPath,
+			),
+			err,
+		)
 	}
 
 	conn := &conn{
@@ -74,7 +83,8 @@ func (c *connector) Connect(ctx context.Context) (driver.Conn, error) {
 	}
 	log := logger.WithContext(conn.id, driverctx.CorrelationIdFromContext(ctx), "")
 
-	log.Info().Msgf("connect: host=%s port=%d httpPath=%s serverProtocolVersion=0x%X", c.cfg.Host, c.cfg.Port, c.cfg.HTTPPath, session.ServerProtocolVersion)
+	log.Info().
+		Msgf("connect: host=%s port=%d httpPath=%s serverProtocolVersion=0x%X", c.cfg.Host, c.cfg.Port, c.cfg.HTTPPath, session.ServerProtocolVersion)
 
 	return conn, nil
 }
@@ -241,7 +251,10 @@ func WithSessionParams(params map[string]string) ConnOption {
 func WithSkipTLSHostVerify() ConnOption {
 	return func(c *config.Config) {
 		if c.TLSConfig == nil {
-			c.TLSConfig = &tls.Config{MinVersion: tls.VersionTLS12, InsecureSkipVerify: true} // #nosec G402
+			c.TLSConfig = &tls.Config{
+				MinVersion:         tls.VersionTLS12,
+				InsecureSkipVerify: true,
+			} // #nosec G402
 		} else {
 			c.TLSConfig.InsecureSkipVerify = true // #nosec G402
 		}
@@ -266,6 +279,12 @@ func WithTransport(t http.RoundTripper) ConnOption {
 func WithCloudFetch(useCloudFetch bool) ConnOption {
 	return func(c *config.Config) {
 		c.UseCloudFetch = useCloudFetch
+	}
+}
+
+func WithCloudFetchHttpClient(httpclient *http.Client) ConnOption {
+	return func(c *config.Config) {
+		c.UserConfig.CloudFetchConfig.HttpClient = httpclient
 	}
 }
 
--- internal/rows/arrowbased/batchloader.go
+++ internal/rows/arrowbased/batchloader.go
@@ -140,6 +140,7 @@ type cloudIPCStreamIterator struct {
 	startRowOffset int64
 	pendingLinks   Queue[cli_service.TSparkArrowResultLink]
 	downloadTasks  Queue[cloudFetchDownloadTask]
+	httpClient     *http.CLient
 }
 
 var _ IPCStreamIterator = (*cloudIPCStreamIterator)(nil)
@@ -162,6 +163,7 @@ func (bi *cloudIPCStreamIterator) Next() (io.Reader, error) {
 			resultChan:         make(chan cloudFetchDownloadTaskResult),
 			minTimeToExpiry:    bi.cfg.MinTimeToExpiry,
 			speedThresholdMbps: bi.cfg.CloudFetchSpeedThresholdMbps,
+			httpClient:         bi.httpClient,
 		}
 		task.Run()
 		bi.downloadTasks.Enqueue(task)
@@ -210,6 +212,7 @@ type cloudFetchDownloadTask struct {
 	link               *cli_service.TSparkArrowResultLink
 	resultChan         chan cloudFetchDownloadTaskResult
 	speedThresholdMbps float64
+  httpClient         *http.Client
 }
 
 func (cft *cloudFetchDownloadTask) GetResult() (io.Reader, error) {
@@ -252,7 +255,7 @@ func (cft *cloudFetchDownloadTask) Run() {
 			cft.link.StartRowOffset,
 			cft.link.RowCount,
 		)
-		data, err := fetchBatchBytes(cft.ctx, cft.link, cft.minTimeToExpiry, cft.speedThresholdMbps)
+		data, err := fetchBatchBytes(cft.ctx, cft.link, cft.minTimeToExpiry, cft.speedThresholdMbps, cft.httpClient)
 		if err != nil {
 			cft.resultChan <- cloudFetchDownloadTaskResult{data: nil, err: err}
 			return
@@ -300,6 +303,7 @@ func fetchBatchBytes(
 	link *cli_service.TSparkArrowResultLink,
 	minTimeToExpiry time.Duration,
 	speedThresholdMbps float64,
+	httpClient *http.Client
 ) (io.ReadCloser, error) {
 	if isLinkExpired(link.ExpiryTime, minTimeToExpiry) {
 		return nil, errors.New(dbsqlerr.ErrLinkExpired)
@@ -317,8 +321,11 @@ func fetchBatchBytes(
 		}
 	}
 
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
 	startTime := time.Now()
-	client := http.DefaultClient
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
